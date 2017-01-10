e10b3b9e88ae0514118ce0b2bf9769ed9806f906
lucvorringa <luca.vornheder@hotmail.de>
2017-01-10 17:29:27 +0100

    first commit

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import time
 import requests
 from pushbullet import Pushbullet
-pb = Pushbullet("o.LxrFcxRbkCHh8AKdEkZPjF3DgYxu1Pvk")
+pb = Pushbullet("API CODE FROM PUSHBULLET")
 
 
 
 
-response = requests.get("http://192.168.178.45/test.php")
+response = requests.get("WEBSITE GOES HERE")
 while response.status_code != 200:
     print ("sleeping:")
     print ("sleeping:")
     time.sleep(5.0)
-    response = requests.get("http://192.168.178.45/test.php")
+    response = requests.get("WEBSITE GOES HERE")
 print ("done!")
-push = pb.push_note("AOS <3", "AOS <3")
+push = pb.push_note("Website is up", "Website is up")
 exit()
